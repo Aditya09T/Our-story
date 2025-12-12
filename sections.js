eb2eb5335adcc4391c509b3d3e8a2d42e88baf83
Aditya09T <adityachowdhury114@gmail.com>
2025-12-12 08:53:51 +0530

    Add alt attributes and new song section in sections.js

--- sections.js
+++ sections.js
@@ -1,17 +1,31 @@
 const SECTIONS = {
     proposal: `
         <h2>💍 Proposal</h2>
-        <img src="images/proposal1.jpg">
-        <img src="images/proposal2.jpg">
+        <img src="images/proposal1.jpg" alt="Proposal Picture 1">
+        <img src="images/proposal2.jpg" alt="Proposal Picture 2">
     `,
 
     school: `
         <h2>🏫 School Days</h2>
-        <img src="images/school1.jpg">
+        <img src="images/school1.jpg" alt="School Picture 1">
+        <p>Science stream, same school 📚</p>
     `,
 
     memory: `
         <h2>📸 Memories</h2>
-        <img src="images/memory1.jpg">
+        <img src="images/memory1.jpg" alt="Memory Picture 1">
+        <p>A collection of our favorite moments together.</p>
+    `,
+
+    song: `
+        <h2>🎧 Our Song - [Replace with Song Title]</h2>
+        <div class="video-container">
+            <iframe src="https://www.youtube.com/embed/YOUR_YOUTUBE_VIDEO_ID?autoplay=1&mute=0&loop=1&playlist=YOUR_YOUTUBE_VIDEO_ID" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+        </div>
+        <p>A song that reminds us of our beginning.</p>
     `
 };
